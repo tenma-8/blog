9d47438f700ae197cac58ec7c14ea3c08cd10233
tenma-8 <sy021004@st.nuis.jp>
2023-07-14 14:25:26 +0900

    モデルリレーションの実装

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\Category;
+
+class PostController extends Controller
+{
+    public function index(Category $category)
+    {
+        return view('categories.index')->with(['posts' => $category->getByCategory()]);
+    }
+    
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,51 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
-use App\Http\Requests\PostRequest;
+use App\Http\Requests\PostRequest; 
+use App\Models\Category;
 
 class PostController extends Controller
 {
-   public function index(Post $post)
-   {
-       return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
-   }
+    public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
+    {
+         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]); 
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function create(Category $category)
+    {
+        return view('posts/create')->with(['categories' => $category->get()]);
+    }
+    
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        // $input += ['user_id' => $request->user()->id];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        // $input_post += ['user_id' => $request->user()->id]; 
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
    
-   public function show(Post $post)
-   {
-      return view('posts/show')->with(['post' => $post]);
-   }
-   
-   public function create()
-   {
-      return view('posts/create');
-   }
-   
-   public function store(PostRequest $request, Post $post)
-   {
-      $input = $request['post'];
-      $post->fill($input)->save();
-      return redirect('/posts/' . $post->id);
-   }
-   
-   public function edit(Post $post)
-   {
-      return view('posts/edit')->with(['post' => $post]);
-   }
-   
-   public function update(PostRequest $request, Post $post)
-   {
-       $input_post = $request['post'];
-       $post->fill($input_post)->save();
 
-       return redirect('/posts/' . $post->id);
-   }
-   
-   public function delete(Post $post)
-   {
-      $post->delete();
-      return redirect('/');
-   }
-   
-   
 }
-
